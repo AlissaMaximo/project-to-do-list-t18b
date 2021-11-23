d0d8c65da50bbe64c444eef72880ac3dd451970c
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-11-23 17:37:41 -0300

    Requisitos 6 e 7 feitos.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-  /* A seguir está o código para criar uma tarefa quando inserido um texto e clicado o botão */
+  /* A seguir está o código para criar uma tarefa quando inserido um texto e clicado o botão. */
   let buttonCreateTask = document.getElementById("criar-tarefa");
+  let taskList = document.getElementById('lista-tarefas');
 
   buttonCreateTask.addEventListener('click', createTask);
 
   function createTask() {
-    let taskList = document.getElementById('lista-tarefas');
     let taskListItem = document.createElement('li');
     let textoTarefa = document.getElementById('texto-tarefa').value;
     
@@ -17,6 +17,12 @@ window.onload = function () {
       document.getElementById('texto-tarefa').value = "";
     }
 
+  /* Alterar o fundo do item clicado para cinza. */
+  taskList.addEventListener('click', changeToGray);
+
+  function changeToGray() {
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
+  }
 
   }
 }
