24ad3faa3820f712eb9112f83b84f65b8c79d913
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-12-01 01:42:32 -0300

    Requisito 8 corrigido para que apenas os itens da lista fiquem cinzas.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = function () {
 /* A seguir está o código para criar uma tarefa quando inserido um texto e clicado o botão. */
   const buttonCreateTask = document.getElementById('criar-tarefa');
-  const taskList = document.getElementById('lista-tarefas');
+  const taskList = document.querySelector('#lista-tarefas');
+  let taskListItems = document.getElementsByTagName('LI');
 
   function createTask() {
     const taskListItem = document.createElement('li');
@@ -21,22 +22,23 @@ window.onload = function () {
 /* Alterar o fundo do item clicado para cinza e qualquer não-clicado para branco. */
   function makeOneGray() {
     function ungray() {
-      const oldSelected = document.querySelector('.selected');
+      let oldSelected = document.querySelector('.selected');
       if (oldSelected != null) {
         oldSelected.classList.remove('selected');
         oldSelected.style.backgroundColor = '';
       }
     }
     function gray() {
-      const clicked = event.target;
-
-      clicked.classList.add('selected');
-      clicked.style.backgroundColor = 'rgb(128, 128, 128)';
+      let clicked = event.target;
+      if (clicked !== taskList) {
+        clicked.classList.add('selected');
+        clicked.style.backgroundColor = 'rgb(128, 128, 128)';
+      }
     }
     ungray();
     gray();
   }
-
+  // tem que fazer que apenas o item li possa ficar cinza, e não a ol também.
   taskList.addEventListener('click', makeOneGray);
 
 };
